dc3382866737b7069eca60b9a529f0ffd09c2ac2
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-10 16:59:50 -0800

    tracking user in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-
+import { useState, useEffect } from 'react';
 import './App.css';
+import { getUser } from './services/fetch-utils';
+import {
+  BrowserRouter as Router,
+  Switch,
+  NavLink,
+  Route,
+  Redirect
+} from 'react-router-dom';
+import AuthPage from './AuthPage';
 
 function App() {
   // track user(state), useEffect(fetch -> getUser), logout func
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    async function fetchUser() {
+      const data = getUser();
+
+      setUser(data);
+    }
+
+    fetchUser();
+  }, []);
+
   return (
     <div className="App">
       hi
--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
+import { signIn, signUp } from './services/fetch-utils';
 
-export default function AuthPage() {
+export default function AuthPage(props) {
     // state (email, pass), handle(signIn, signUp)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSignIn(e) {
+    e.preventDefault();
+    
+    const user = await signIn(email, password);
+
+  }
+
+  async function handleSignUp(e) {
+    e.preventDefault();
+
+  }
+
   return (
     <div>AuthPage
       {/* form w/ 2 buttons */}
