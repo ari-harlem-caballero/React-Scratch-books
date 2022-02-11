5f0519eb10936776f1cdb46c78057d87e2864c0c
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-11 09:29:11 -0800

    created Book.js

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Book() {
-  // LINK to detail page, hols detail info (title, author, etc) IE: single div in ListPage
+export default function Book({ book }) {
+  // LINK to detail page, holds detail info (title, author, etc) IE: single div in ListPage
   return (
-    <div>Book</div>
+    <Link to={`/book-list/${book.id}`}>
+      <div className='book-list'>
+        <h2>
+          {book.title}
+        </h2>
+        <p className='list-author'>
+          {book.author}
+        </p>
+        <p className='list-genre'>
+          {book.genre}
+        </p>
+      </div>
+    </Link>
   );
 }
