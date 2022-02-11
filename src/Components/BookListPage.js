d8a646ea552327d600f8c1fb1252d776681b2665
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-11 09:35:39 -0800

    track bookArr in BookListPage.js

--- src/Components/BookListPage.js
+++ src/Components/BookListPage.js
@@ -4,6 +4,18 @@ import { getAllBooks } from '../services/fetch-utils';
 
 export default function BookListPage() {
   // array of books, useEffect(fetch -> getBooks), MAP over arr
+  const [bookArr, setBookArr] = useState([]);
+
+  useEffect(() => {
+    async function fetchBooks() {
+      const data = await getAllBooks();
+
+      setBookArr(data);
+    }
+
+    fetchBooks();
+  }, []);
+
   return (
     <div>BookListPage</div>
   );
