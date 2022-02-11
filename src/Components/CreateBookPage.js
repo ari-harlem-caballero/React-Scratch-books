e6dfc13db36a8ea6286c20e50470a2e4a8d75e20
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-11 11:33:16 -0800

    set state, created handler for CreateBook.js

--- src/Components/CreateBookPage.js
+++ src/Components/CreateBookPage.js
@@ -4,9 +4,31 @@ import { createBook } from '../services/fetch-utils';
 
 export default function CreateBookPage() {
   // state for all elems/history, handleSubmit(await createBook, history.push)
+  const [bookTitle, setBookTitle] = useState('');
+  const [bookAuthor, setBookAuthor] = useState('');
+  const [bookGenre, setBookGenre] = useState('');
+  const [bookSeries, setBookSeries] = useState(false);
+  const [bookDate, setBookDate] = useState();
+  const [bookDescription, setBookDescription] = useState('');
+  const history = useHistory();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    await createBook({
+      title: bookTitle,
+      author: bookAuthor,
+      genre: bookGenre,
+      release_date: bookDate,
+      series: bookSeries,
+      description: bookDescription,
+    });
+
+    history.push('./book-list');
+  }
 
   return (
-    <div>CreateBookPage
+    <div className='create'>
       {/* form(handleSub): all elems (onChange e.target), button */}
     </div>
   );
