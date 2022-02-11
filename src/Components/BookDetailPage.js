f437e42dadfc165d34a3d86985b3148f7c979da7
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-11 15:34:22 -0800

    wrote book detail page

--- src/Components/BookDetailPage.js
+++ src/Components/BookDetailPage.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { useRouteMatch } from 'react-router-dom/cjs/react-router-dom.min';
+import { getBookById } from '../services/fetch-utils';
 
 export default function BookDetailPage() {
   // state (singleBook, useRouteMatch), useEffect(fetch -> getBookID)
+  const [bookDetail, setBookDetail] = useState({});
+  const match = useRouteMatch();
+
+  useEffect(() => {
+    async function fetchSingleBook() {
+      const data = await getBookById(match.params.id);
+
+      setBookDetail(data);
+    }
+
+    fetchSingleBook();
+  }, [match]);
+
   return (
-    <div>BookDetailPage
+    <div className='detail'>
       {/* detail page elems (title, author, publisher, etc) */}
+      <h1>{bookDetail.title}</h1>
+      <h2>{bookDetail.author}</h2>
+      <p>{bookDetail.release_year}</p>
+      <div className='book-info'>
+        <p>
+          Genre:
+          {bookDetail.genre}
+        </p>
+        <p>
+          Series:
+          {/* boolean needs to render as STRING */}
+          {bookDetail.series ? 'yes' : 'no'}
+        </p>
+      </div>
+      <p>{bookDetail.description}</p>
     </div>
   );
 }
